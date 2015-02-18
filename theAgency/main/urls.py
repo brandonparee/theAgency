b296ec7bf7717da364233c1a1579528e6454820b
Mark Y. Thompson Jr <moomoomamoo@gmail.com>
2015-02-18 03:04:19 -0700

    handled if there doesn't exist an id for a compaign, also handled if there is no giveaways for that compaign.

--- theAgency/main/urls.py
+++ theAgency/main/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'main.views.home', name='home'),
-    url(r'^makeAgency/$', 'main.views.makeAgency', name='makeAgency'),
+    url(r'^makeCompaign/$', 'main.views.makeCompaign', name='makeCompaign'),
     url(r'^makeAccount/$', 'main.views.makeAccount', name='makeAccount'),
     url(r'^makeGiveaway/$', 'main.views.makeGiveaway', name='makeGiveaway'),
      url(r'^compaigns/?', 'main.views.compaigns', name='compaigns'),
--- theAgency/main/views.py
+++ theAgency/main/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render#, redirect
 from django.http import HttpResponseRedirect
 #from django.template import RequestContext
 #from django.forms import ModelForm
-from main.models import Compaign
+from main.models import Compaign, Giveaway
 from main.forms import CompaignForm, AccountForm, GiveawayForm
 
 def home(request):
 	return render(request, 'main/index.html', {})
 
-def makeAgency(request):
+def makeCompaign(request):
   if request.method == 'POST':
     form = CompaignForm(request.POST)
     if form.is_valid():
@@ -40,7 +40,15 @@ def makeGiveaway(request):
 	return render(request, 'main/makeGiveaway.html', {'form': form})
 
 def compaign(request, id_num):
-    return render(request, 'main/compaign.html', {'Compaign': Compaign.objects.get(id=id_num)})
+  try:
+    compaigns = Compaign.objects.get(id=id_num)
+  except Compaign.DoesNotExist:
+    return HttpResponseRedirect('/compaigns/')
+  try:
+    giveaways = Giveaway.objects.get(compaign_fk=id_num)
+  except Giveaway.DoesNotExist:
+    giveaways = None
+  return render(request, 'main/compaign.html', {'Compaign': Compaign.objects.get(id=id_num), 'Giveaways': giveaways})
 
 def compaigns(request):
-    return render(request, 'main/compaigns.html', {'Compaigns': Compaign.objects.all()})
+  return render(request, 'main/compaigns.html', {'Compaigns': Compaign.objects.all()})
