b0966e9741610e28c58d62a98ff222b5d3cb2ee9
Mark Y. Thompson Jr <moomoomamoo@gmail.com>
2015-02-18 21:15:06 -0700

    removed giveaway table, going to mix this idea of a giveaway table with each campaign table

--- theAgency/main/models/giveaway.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Giveaway(models.Model):
-	quantity = models.CharField(max_length=200)
-	item = models.CharField(max_length=200)
-	image = models.CharField(max_length=200)
-	description = models.CharField(max_length=2000)
-	campaign_fk = models.ForeignKey('Campaign')
