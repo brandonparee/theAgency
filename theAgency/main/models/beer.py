cd0b69af921257643cb3bae83af33a0611acef98
Mark Y. Thompson Jr <moomoomamoo@gmail.com>
2015-02-18 23:17:43 -0700

    redoing tables, there's 4 campaigns now.

---
+++ theAgency/main/models/beer.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class CampaignBeer(models.Model):
+	name=models.CharField(max_length = 200)
+	email=models.EmailField(max_length = 75)
+	birth=models.CharField(min_length=5, max_length=5)
