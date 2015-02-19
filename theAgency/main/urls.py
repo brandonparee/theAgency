b1dc194fade6ac3bf1f7ba56856d0ad3cc005682
Brandon P <brandonp@nau.edu>
2015-02-19 08:13:22 -0700

    PEP8 stuff. Got the webserver to run.

--- theAgency/main/urls.py
+++ theAgency/main/urls.py
@@ -5,16 +5,24 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'main.views.home', name='home'),
-    url(r'^campaigns/$', 'main.views.campaigns', name='campaigns'),
-    url(r'^signup/$', 'main.views.email_form', name='email_form'),
-    url(r'^giveaway/beer/$', 'main.views.beer_form', name='beer_form'),
-    url(r'^giveaway/language/$', 'main.views.language_form', name='language_form'),
-    url(r'^giveaway/shirt/$', 'main.views.shirt_form', name='shirt_form'),
-    url(r'^email/$', 'main.views.email_form', name='email_form'),
-    url(r'^emails/?', 'main.views.all_emails', name='all_emails'),
-    url(r'^thanks-for-your-input/?', 'main.views.thanks', name='thanks'),
-    url(r'^admin/', include(admin.site.urls)),
-) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                       # Examples:
+                       # url(r'^blog/', include('blog.urls')),
+                       url(r'^$', 'main.views.home', name='home'),
+                       url(r'^campaigns/$', 'main.views.campaigns',
+                           name='campaigns'),
+                       url(r'^signup/$', 'main.views.email_form',
+                           name='email_form'),
+                       url(r'^giveaway/beer/$',
+                           'main.views.beer_form', name='beer_form'),
+                       url(r'^giveaway/language/$',
+                           'main.views.language_form', name='language_form'),
+                       url(r'^giveaway/shirt/$',
+                           'main.views.shirt_form', name='shirt_form'),
+                       url(r'^email/$', 'main.views.email_form',
+                           name='email_form'),
+                       url(r'^emails/?', 'main.views.all_emails',
+                           name='all_emails'),
+                       url(r'^thanks-for-your-input/?',
+                           'main.views.thanks', name='thanks'),
+                       url(r'^admin/', include(admin.site.urls)),
+                       ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- theAgency/main/views.py
+++ theAgency/main/views.py
@@ -4,54 +4,62 @@ from django.http import HttpResponseRedirect
 from main.models import Email, Beer, Language, Shirt
 from main.forms import EmailForm, BeerForm, LanguageForm, ShirtForm
 
+
 def home(request):
-	return render(request, 'main/index.html', {})
+    return render(request, 'main/index.html', {})
+
 
 def email_form(request):
-  if request.method == 'POST':
-    form = EmailForm(request.POST)
-    if form.is_valid():
-    	form.save()
-    	return HttpResponseRedirect('/thanks-for-your-input/')
-  else:
-    form = EmailForm()
-  return render(request, 'main/email_form.html', {'form': form})
+    if request.method == 'POST':
+        form = EmailForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/thanks-for-your-input/')
+    else:
+        form = EmailForm()
+    return render(request, 'main/email_form.html', {'form': form})
+
 
 def beer_form(request):
-  if request.method == 'POST':
-    form = BeerForm(request.POST)
-    if form.is_valid():
-    	form.save()
-    	return HttpResponseRedirect('/thanks-for-your-input/')
-  else:
-    form = BeerForm()
-  return render(request, 'main/beer_form.html', {'form': form})
+    if request.method == 'POST':
+        form = BeerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/thanks-for-your-input/')
+    else:
+        form = BeerForm()
+    return render(request, 'main/beer_form.html', {'form': form})
+
 
 def language_form(request):
-	if request.method == 'POST':
-		form = LanguageForm(request.POST)
-		if form.is_valid():
-			form.save()
-			return HttpResponseRedirect('/thanks-for-your-input/')
-	else:
-		form = LanguageForm()
-	return render(request, 'main/language_form.html', {'form': form})
+    if request.method == 'POST':
+        form = LanguageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/thanks-for-your-input/')
+    else:
+        form = LanguageForm()
+    return render(request, 'main/language_form.html', {'form': form})
+
 
 def shirt_form(request):
-  if request.method == 'POST':
-    form = ShirtForm(request.POST)
-    if form.is_valid():
-      form.save()
-      return HttpResponseRedirect('/thanks-for-your-input/')
-  else:
-    form = ShirtForm()
-  return render(request, 'main/shirt_form.html', {'form': form})
+    if request.method == 'POST':
+        form = ShirtForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/thanks-for-your-input/')
+    else:
+        form = ShirtForm()
+    return render(request, 'main/shirt_form.html', {'form': form})
+
 
 def all_emails(request):
-  return render(request, 'main/all_emails.html', {'emails': Email.objects.all()})
+    return render(request, 'main/all_emails.html', {'emails': Email.objects.all()})
+
 
 def campaigns(request):
-  return render(request, 'main/campaigns.html', {'campaigns': })
+    return render(request, 'main/campaigns.html')
+
 
 def thanks(request):
-  return render(request, 'main/thanks.html', {})
+    return render(request, 'main/thanks.html', {})
