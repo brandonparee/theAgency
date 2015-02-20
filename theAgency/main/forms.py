952f469f98a623eb88e2e3f8ce6242a0f59bc005
brandonparee <brandonp@nau.edu>
2015-02-19 21:46:01 -0700

    Added a placeholder to show correct formatting for a date.

--- theAgency/main/forms.py
+++ theAgency/main/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 from main.models import Email, Beer, Language, Shirt
 
+date_widget =  {
+            'birth': forms.TextInput(attrs={'placeholder': 'MM/DD/YYYY'}),
+        }
+
 class EmailForm(forms.ModelForm):
 	class Meta:
 		model = Email
@@ -9,14 +13,17 @@ class EmailForm(forms.ModelForm):
 class BeerForm(forms.ModelForm):
 	class Meta:
 		model = Beer
+		widgets = date_widget
 		fields = ['name', 'email', 'birth', 'zipcode']
 
 class LanguageForm(forms.ModelForm):
 	class Meta:
 		model = Language
+		widgets = date_widget
 		fields = ['name', 'email', 'language', 'race', 'birth', 'zipcode']
 
 class ShirtForm(forms.ModelForm):
 	class Meta:
 		model = Shirt
+		widgets = date_widget
 		fields = ['name', 'email', 'shirt_size', 'color', 'cut', 'zipcode']
